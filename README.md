05da7dea15b7b7dff20205393e5e4f9d6bdc9211
Ashish Panigrahi <panigrahi.ashish1999@gmail.com>
2020-05-12 00:04:58 +0000

    Updated statusbar for aesthetics

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # dwmblocks
 Modular status bar for dwm written in c.
-# modifying blocks
+
+# Modifying blocks
 The statusbar is made from text output from commandline programs.
 Blocks are added and removed by editing the blocks.h header file.
+
+## Installation
+```
+git clone https://github.com/paniash/dwmblocks
+cd dwmblocks
+sudo make clean install
+```
+
--- blocks.h
+++ blocks.h
@@ -1,16 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+    { "",       "dwm_music",        1,                   11},
+    /* { "",       "dwm_arch_update",     1800,                   8}, */
+    { " ",       "dwm_volume",        1,                   10},
+    /* { "",       "dwm_weather",       3600,                   5}, */
     /* { "",     "dwm_network",         5,                      4}, */
-    /* { " ",       "dwm_music",        0,                   11}, */
-    { "",       "dwm_volume",        1,                   10},
-    { "",       "dwm_weather",       18000,                   5},
-    /* { " ",       "dwm_brightness",     0,                     9}, */
-    { " ",       "dwm_cpu",          10,                     13},
-    { " ",       "dwm_memory",          10,                     14},
+    { "  ",       "dwm_brightness",     1,                     9},
     /* { " ",       "dwm_news",          0,                     6}, */
+    { " ",       "dwm_cpu",          10,                     13},
+    { "",       "dwm_memory",          10,                     14},
     { " ",       "dwm_battery",       20,                     0},
-    { "  ",     "dwm_date",         18000,                      0},
+    { "  ",     "dwm_date",         180,                      0},
     /* { "  ",     "dwm_time",         60,                      0} */
     { "  ",     "dwm_time",         60,                      0}
 };
