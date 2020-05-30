3725b72b56a0c8afb8013741bdb0ca3d2532ad09
Ashish Panigrahi <panigrahi.ashish1999@gmail.com>
2020-05-30 19:21:22 +0000

    Latest build

--- blocks.h
+++ blocks.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    { "",       "dwm_music",        1,                   11},
+    { "",       "dwm_music",        0,                   11},
     /* { "",       "dwm_arch_update",     1800,                   8}, */
-    { " ",       "dwm_volume",        1,                   10},
+    { " ",       "dwm_volume",        0,                   10},
     /* { "",       "dwm_weather",       3600,                   5}, */
-    { "  ",       "dwm_brightness",     1,                     9},
+    { "  ",       "dwm_brightness",     0,                     9},
     { "",     "dwm_mail",         180,                      12},
-    /* { " ",       "dwm_news",          0,                     6}, */
+    /* { "",       "dwm_news",          180,                     6}, */
     { " ",       "dwm_cpu",          10,                     13},
     /* { "",       "dwm_nettraf",       1,                     16}, */
     { "",       "dwm_memory",          10,                     14},
