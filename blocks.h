ef4e89f05baf03e79eefa2e085e75e0a5f4f31fa
Ashish Panigrahi <panigrahi.ashish1999@gmail.com>
2020-06-09 23:03:36 +0000

    Added redshift and minor updates

--- blocks.h
+++ blocks.h
@@ -4,18 +4,19 @@ static const Block blocks[] = {
     { "",       "dwm_music",        0,                   11},
     /* { "",       "dwm_arch_update",     1800,                   8}, */
     { " ",       "dwm_volume",        0,                   10},
-    /* { "",       "dwm_weather",       3600,                   5}, */
+    /* { "",       "dwm_weather",       1800,                   5}, */
     { "  ",       "dwm_brightness",     0,                     9},
+    { "",     "dwm_redshift",         900,                      20},
     { "",     "dwm_mail",         180,                      12},
+    /* { "",     "dwm_torrent",         20,                      7}, */
     /* { "",       "dwm_news",          180,                     6}, */
     { " ",       "dwm_cpu",          10,                     13},
     /* { "",       "dwm_nettraf",       5,                     16}, */
     { "",       "dwm_memory",          10,                     14},
     /* { "",     "dwm_internet",         5,                      4}, */
-    { " ",       "dwm_battery",       20,                     0},
-    { "  ",     "dwm_date",         300,                      0},
-    /* { "  ",     "dwm_time",         60,                      0} */
-    { "  ",     "dwm_time",         60,                      0}
+    { " ",       "dwm_battery",       20,                     9},
+    { "  ",     "dwm_date",         300,                      17},
+    { "  ",     "dwm_time",         60,                      18}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
