e0c42c1a6524f66f32c3882c62591d78dd3afcd3
Ashish Panigrahi <panigrahi.ashish1999@gmail.com>
2020-06-13 00:51:31 +0000

    Updated calendar icon

--- blocks.h
+++ blocks.h
@@ -5,8 +5,8 @@ static const Block blocks[] = {
     /* { "",       "dwm_arch_update",     1800,                   8}, */
     { " ",       "dwm_volume",        0,                   10},
     /* { "",       "dwm_weather",       1800,                   5}, */
-    { "  ",       "dwm_brightness",     0,                     9},
-    { "",     "dwm_redshift",         900,                      20},
+    { "  ",       "dwm_brightness",     0,                     9},
+    { "",     "dwm_redshift",         0,                      20},
     { "",     "dwm_mail",         180,                      12},
     /* { "",     "dwm_torrent",         20,                      7}, */
     /* { "",       "dwm_news",          180,                     6}, */
@@ -15,7 +15,7 @@ static const Block blocks[] = {
     { "",       "dwm_memory",          10,                     14},
     /* { "",     "dwm_internet",         5,                      4}, */
     { " ",       "dwm_battery",       20,                     9},
-    { "  ",     "dwm_date",         300,                      17},
+    { "  ",     "dwm_date",         300,                      17},
     { "  ",     "dwm_time",         60,                      18}
 };
 
