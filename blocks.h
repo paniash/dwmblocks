fa9e539868921924079c8b47c240a5af6bac7583
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2020-11-15 21:41:50 +0530

    made code legible

--- blocks.h
+++ blocks.h
@@ -2,22 +2,22 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
     { "",       "music",        0,                   11},
-    { "",     "temp",         2700,                      21},
-    /* { "",       "arch_update",     1800,                   8}, */
     { "",       "volume",        0,                   10},
-    /* { "",       "duptime",        600,                   19}, */
-    /* { "",       "weather",       1800,                   5}, */
     { "",       "brightness",     0,                     9},
     { "",     "nmail",         180,                      12},
-    /* { "",     "torrent",         20,                      7}, */
-    /* { "",       "news",          180,                     6}, */
     { "",       "cpu",          10,                     13},
-    /* { "",       "nettraf",       5,                     16}, */
-    { "",       "memory",          10,                     14},
-    /* { "",     "internet",         5,                      4}, */
+    { "",       "memory",          15,                     14},
     { "",       "battery",       20,                     9},
     { "",     "ddate",         300,                      17},
     { "",     "dtime",         60,                      18}
+    /* { "",       "arch_update",     1800,                   8}, */
+    /* { "",       "duptime",        600,                   19}, */
+    /* { "",       "weather",       1800,                   5}, */
+    /* { "",     "torrent",         20,                      7}, */
+    /* { "",       "news",          180,                     6}, */
+    /* { "",       "nettraf",       5,                     16}, */
+    /* { "",     "internet",         5,                      4}, */
+    /* { "",     "temp",         2700,                      21}, */
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
