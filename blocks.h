06e61b89a85a1f33db2015d0b707de05bde3affc
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2023-03-11 12:16:58 +0000

    updated

--- blocks.h
+++ blocks.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    { "",     "smusic",        0,                   11},
-    { "",     "svolume",       0,                   10},
-    { "",     "brightness",    0,                    9},
-    { "",     "battery",       20,                   9},
-    { "",     "sdate",         0,                   17},  // updates through crontab
-    { "",     "stime",         60,                  18}
+    { "",     "smusic",        0,                  11},
+    { "",     "pipevolume",    0,                  10},
+    { "",     "brightness",    0,                  19},
+    { "",     "battery",       20,                  9},
+    { "",     "sdate",         0,                  17},  // updates through crontab
+    { "",     "stime",         60,                 18}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
